63ddad1483b597d4c884dfff6e1c2e789efd609e
Oleksandr Havrylko <oleksandr.s.havrylko@globallogic.com>
2018-11-07 14:52:18 +0200

    Updates

---
+++ precache-manifest.73b8898e997270146d5f4f7bf8ae6119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "2d485f1e8d5d026953a0",
+    "url": "/react-app/static/js/main.2d485f1e.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/react-app/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "2d485f1e8d5d026953a0",
+    "url": "/react-app/static/css/main.7b1fed7e.chunk.css"
+  },
+  {
+    "revision": "e9af8d021aa37c53d2ee18804b7f4ede",
+    "url": "/react-app/index.html"
+  }
+];
